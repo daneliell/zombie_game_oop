031e035554ce8797507786e329db56d390cbd694
Sravan Krsna Rao <sven0012@student.monash.edu>
2020-05-17 03:54:56 +0800

    Updated Farmer

--- src/game/Farmer.java
+++ src/game/Farmer.java
@@ -1,12 +1,29 @@
 package game;
 
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actions;
+import edu.monash.fit2099.engine.Display;
+import edu.monash.fit2099.engine.DoNothingAction;
+import edu.monash.fit2099.engine.GameMap;
+
 public class Farmer extends Human {
-	
+	private Behaviour[] behaviours = {new WanderBehaviour(),
+									  new FarmerBehaviour()
+	};
+									 
 	
 	protected Farmer(String name, char displayChar, int hitPoints) {
 		super(name, 'F', 50);
 	}
 	
+	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
+		for (Behaviour behaviour : behaviours) {
+			Action action = behaviour.getAction(this, map);
+			if (action != null)
+				return action;
+		}
+		return new DoNothingAction();
+	}
 	
 
 }
