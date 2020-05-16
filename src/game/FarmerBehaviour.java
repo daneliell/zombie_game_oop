1236a2a871eadf0d853c368790042421b5790461
Sravan Krsna Rao <sven0012@student.monash.edu>
2020-05-17 03:46:48 +0800

    Updated FarmerBehaviour

--- src/game/FarmerBehaviour.java
+++ src/game/FarmerBehaviour.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Ground;
 import edu.monash.fit2099.engine.Location;
 
 public class FarmerBehaviour implements Behaviour {
 	private int minProb = 1;
 	private int maxProb = 100;
 	private int sowProbability;
+	private Random random;
 	
 	public FarmerBehaviour() {
 		Random rand = new Random();
@@ -21,20 +24,32 @@ public class FarmerBehaviour implements Behaviour {
 		ArrayList<Action> actions = new ArrayList<Action>();
 		
 		Location farmerLocation = map.locationOf(actor);
-		Location cropLocation = new Location(map, farmerLocation.x()+1, farmerLocation.y());
+		Location nextLocation = new Location(map, farmerLocation.x()+1, farmerLocation.y());
 		
 		if(this.sowProbability <= 33) {
-			actions.add(new SowAction(cropLocation));
+			actions.add(new SowAction(nextLocation));
 			
 		}
 		
-		if(farmerLocation.getDisplayChar() == 'c') {
-			Crop crop = farmerLocation.getGround();
-			}
+		if(farmerLocation.getGround() instanceof Crop) {
+			actions.add(new FertilizeAction((Crop) farmerLocation.getGround()));
+			
+		}
+		
+		if(farmerLocation.getGround() instanceof Crop) {
+			actions.add(new HarvestAction((Crop) farmerLocation.getGround(), farmerLocation));
+		}
+		else if(nextLocation.getGround() instanceof Crop) {
+			actions.add(new HarvestAction((Crop) nextLocation.getGround(), nextLocation));
+		}
+		
+		if (!actions.isEmpty()) {
+			return actions.get(random.nextInt(actions.size()));
+		}
+		else {
+			return null;
+		}
 		
 	}
-	
-	
-	
 
 }
