ff116e19af8cc320370baa7e3e9ae0d7841a8922
Daniel Yuen <dyue0003@student.monash.edu>
2020-05-16 16:40:04 +0800

    Updated ZombieCorpse

--- src/game/ZombieCorpse.java
+++ src/game/ZombieCorpse.java
@@ -1,7 +1,11 @@
 package game;
 
-import fit2099.edu.engine.Item;
+import edu.monash.fit2099.engine.Item;
 
 public class ZombieCorpse extends Item {
+	
+	public ZombieCorpse(String name, char displayChar, boolean portable) {
+		super("dead" + name, '%', false);
+	}
 
 }
