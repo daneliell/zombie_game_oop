9cb749183d52149863f2b1154a8f2a4243be4862
Daniel Yuen <dyue0003@student.monash.edu>
2020-05-16 15:25:01 +0800

    Updated ZombieArm and ZombieLeg

--- src/game/ZombieArm.java
+++ src/game/ZombieArm.java
@@ -1,5 +1,17 @@
 package game;
 
-public class ZombieArm {
+import edu.monash.fit2099.engine.WeaponItem;
+
+/**
+ * Weapon dropped from knocking off the arm of a Zombie. Can be crafted into a Zombie Club.
+ * 
+ * @author Daniel Yuen
+ *
+ */
+public class ZombieArm extends WeaponItem{
+	
+	public ZombieArm() {
+		super("zombie arm", '/', 15, "whacks");
+	}
 
 }
--- src/game/ZombieLeg.java
+++ src/game/ZombieLeg.java
@@ -1,5 +1,17 @@
 package game;
 
-public class ZombieLeg {
+import edu.monash.fit2099.engine.WeaponItem;
+
+/**
+ * Weapon dropped from knocking off the leg of a Zombie. Can be crafted into a Zombie Mace.
+ * 
+ * @author Daniel Yuen
+ *
+ */
+public class ZombieLeg extends WeaponItem {
+	
+	public ZombieLeg() {
+		super("zombie leg", '}', 20, "whacks");
+	}
 
 }
