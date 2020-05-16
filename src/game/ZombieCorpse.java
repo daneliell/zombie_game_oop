22c0edf393f31edc9d1f5162eab417ddd9e05cf6
Daniel Yuen <dyue0003@student.monash.edu>
2020-05-16 16:51:03 +0800

    Completed ZombieCorpse Class

--- src/game/ZombieCorpse.java
+++ src/game/ZombieCorpse.java
@@ -1,11 +1,32 @@
 package game;
 
 import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.Location;
+
+import java.util.Random;
 
 public class ZombieCorpse extends Item {
 	
+	private int conversionCounter;
+	private int minTurns = 5;
+	private int maxTurns = 10;
+	
 	public ZombieCorpse(String name, char displayChar, boolean portable) {
 		super("dead" + name, '%', false);
+		
+		Random rand = new Random();
+		conversionCounter = rand.nextInt((maxTurns - minTurns) + 1) + minTurns;
 	}
-
+	
+	@Override
+	public void tick(Location currentLocation) {
+		if (conversionCounter == 0) {
+			currentLocation.removeItem(this);
+			currentLocation.addActor(new Zombie(name));
+		}
+		else {
+			conversionCounter--;
+		}
+	}
+	
 }
