4ed8dd81335dde3299d1848e0e753beac19a72ae
Sravan Krsna Rao <sven0012@student.monash.edu>
2020-05-18 23:41:40 +0800

    Updated ZombieArm, ZombieLeg, ItemInterface, CraftingAction classes

--- src/edu/monash/fit2099/interfaces/ItemInterface.java
+++ src/edu/monash/fit2099/interfaces/ItemInterface.java
@@ -1,9 +1,18 @@
 package edu.monash.fit2099.interfaces;
 
+import java.util.ArrayList;
+
+import edu.monash.fit2099.engine.Item;
+
 /**
  * This interface provides the ability to add methods to Ground, without modifying code in the engine,
  * or downcasting references in the game.   
  */
 public interface ItemInterface {
+	
+	
+	public void setCraftItems(ArrayList<Item> craftItems);
+	public void addCraftItems(Item item);
+	public ArrayList<Item> getCraftItems();
 
 }
