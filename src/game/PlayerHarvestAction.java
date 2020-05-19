2b9b19b364bf32d1da13db5a18dd83f7a521c6ae
Sravan Krsna Rao <sven0012@student.monash.edu>
2020-05-20 04:04:54 +0800

    Updated HarvestAction and PlayerHarvestAction classes

--- src/game/PlayerHarvestAction.java
+++ src/game/PlayerHarvestAction.java
@@ -4,28 +4,25 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Location;
+
 /**
  * 
  * @author Sravan
  *
  */
-public class HarvestAction extends Action {
+public class PlayerHarvestAction extends HarvestAction{
 	protected Crop crop;
 	protected Location ground;
 	
-	public HarvestAction(Crop crop, Location ground) {
-		this.crop = crop;
-		this.ground = ground;
+	public PlayerHarvestAction(Crop crop, Location ground) {
+		super(crop, ground);
 	}
 	
 	public String execute(Actor actor, GameMap map) {
 			this.ground.setGround(new Dirt());
-			this.ground.addItem(new Food());
-			return actor + " harvested the ripe " + this.crop;
-	}
-	
-	public String menuDescription(Actor actor) {
-		return actor + " harvested food from the " + this.crop;
-	}
+			actor.addItemToInventory(new Food());
+			return super.menuDescription(actor);
+		}
+		
 
 }
