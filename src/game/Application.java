ce63efc5f8a706f9916e5e0eafe6d704e1daff6f
Sravan Krsna Rao <sven0012@student.monash.edu>
2020-05-21 00:48:38 +0800

    Pushed application

--- src/game/Application.java
+++ src/game/Application.java
@@ -78,13 +78,12 @@ public class Application {
 		gameMap.at(74, 20).addItem(new Plank());
 		
 		 //FIXME: Add more zombies!
-		//gameMap.at(30, 20).addActor(new Zombie("Groan"));
-		//gameMap.at(30,  18).addActor(new Zombie("Boo"));
-		//gameMap.at(10,  4).addActor(new Zombie("Uuuurgh"));
-		//gameMap.at(50, 18).addActor(new Zombie("Mortalis"));
-		//gameMap.at(1, 10).addActor(new Zombie("Gaaaah"));
-		//gameMap.at(62, 12).addActor(new Zombie("Aaargh"));	
-		gameMap.at(42, 16).addItem(new ZombieLeg());
+		gameMap.at(30, 20).addActor(new Zombie("Groan"));
+		gameMap.at(30,  18).addActor(new Zombie("Boo"));
+		gameMap.at(10,  4).addActor(new Zombie("Uuuurgh"));
+		gameMap.at(50, 18).addActor(new Zombie("Mortalis"));
+		gameMap.at(1, 10).addActor(new Zombie("Gaaaah"));
+		gameMap.at(62, 12).addActor(new Zombie("Aaargh"));	
 		world.run();
 	}
 }
