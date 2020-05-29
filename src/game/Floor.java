0b3da1222fc52734a5e8250291f29228db2ed0d5
Daniel Yuen <dyue0003@student.monash.edu>
2020-05-29 17:37:31 +0800

    Added Floor and Path classes

---
+++ src/game/Floor.java
@@ -0,0 +1,22 @@
+package game;
+
+import edu.monash.fit2099.engine.Ground;
+
+public class Floor extends Ground{
+	
+	public Floor() {
+		super(',');
+	}
+	
+	@Override
+	public Crop asCrop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean isRipe() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/game/Path.java
@@ -0,0 +1,22 @@
+package game;
+
+import edu.monash.fit2099.engine.Ground;
+
+public class Path extends Ground{
+	
+	public Path() {
+		super(':');
+	}
+
+	@Override
+	public Crop asCrop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean isRipe() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
