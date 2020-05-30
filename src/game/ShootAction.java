3dace79f27940859456f93586f2644a7761e7247
Daniel Yuen <dyue0003@student.monash.edu>
2020-05-30 15:45:32 +0800

    Updated Shotgun and ShootAction

--- src/game/ShootAction.java
+++ src/game/ShootAction.java
@@ -1,5 +1,12 @@
 package game;
 
+import edu.monash.fit2099.engine.Actor;
+
 public class ShootAction extends AttackAction {
 
+	public ShootAction(Actor target) {
+		super(target);
+		// TODO Auto-generated constructor stub
+	}
+	
 }
--- src/game/Shotgun.java
+++ src/game/Shotgun.java
@@ -1,6 +1,8 @@
 package game;
 
+import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.Location;
 import edu.monash.fit2099.engine.WeaponItem;
 
 public class Shotgun extends WeaponItem {
@@ -9,7 +11,13 @@ public class Shotgun extends WeaponItem {
 		super("shotgun", '>', 40, "blasts");
 	}
 	
-	
+	@Override
+	public void tick(Location currentLocation, Actor actor) {
+		if (actor.getInventory()) {
+			
+		}
+		this.allowableActions.add(ShootAction());
+	}
 
 	@Override
 	public Item getCraftItem() {
