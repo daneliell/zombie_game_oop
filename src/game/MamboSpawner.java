7a591b84f934a2e548c66fff60aa399e3b1dbb08
Sravan Krsna Rao <sven0012@student.monash.edu>
2020-05-30 20:14:15 +0800

    Created MamboSpawner class

---
+++ src/game/MamboSpawner.java
@@ -0,0 +1,36 @@
+package game;
+
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.Location;
+
+public class MamboSpawner extends Item {
+	private boolean spawned = false;
+	private MamboMarie marie = new MamboMarie();
+	
+	public MamboSpawner() {
+		super("Mambo Spawner", '.', false);
+	}
+	
+	public void tick(Location currentLocation) {
+		if(spawned) {
+			if (marie.getSpawnCounter()%10==0) {
+				GameMap currentMap = marie.getCurrentMap();
+				currentMap.removeActor(marie);
+				spawned = false;
+			}
+		}
+		else {
+			if(Math.random()<0.05) {
+				currentLocation.addActor(marie);
+				spawned = true;
+			}
+		}
+	}
+
+	@Override
+	public Item getCraftItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
