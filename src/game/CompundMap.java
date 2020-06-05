44785594fd782734259fd53262090bccac6df4b8
Sravan Krsna Rao <sven0012@student.monash.edu>
2020-06-06 04:12:17 +0800

    Created CompundMap class
    A subclass of the GameMap class specifically for the compound map

---
+++ src/game/CompundMap.java
@@ -0,0 +1,21 @@
+package game;
+
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.GroundFactory;
+
+public class CompundMap extends GameMap {
+	private boolean MamboStatus;
+	
+	public CompundMap(GroundFactory groundFactory, char groundChar, int width, int height) {
+		super(groundFactory, groundChar, width, height);
+		this.MamboStatus = true;
+	}
+	
+	public void setMamboStatus(boolean status) {
+		this.MamboStatus = status;
+	}
+	
+	public boolean getMamboStatus() {
+		return this.MamboStatus;
+	}
+}
