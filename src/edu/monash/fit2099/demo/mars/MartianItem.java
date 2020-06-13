6b7e434a8d8f1bf02b64591bf219b1456be8f33a
Daniel Yuen <dyue0003@student.monash.edu>
2020-06-13 12:38:38 +0800

    Touched up design docs
    Removed extra imports from certain files as well

--- src/edu/monash/fit2099/demo/mars/MartianItem.java
+++ src/edu/monash/fit2099/demo/mars/MartianItem.java
@@ -1,11 +1,7 @@
 package edu.monash.fit2099.demo.mars;
 
-import java.util.ArrayList;
-
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Item;
-import game.CraftingAction;
-
 public class MartianItem extends Item{
 
 	public MartianItem(String name, char displayChar, boolean portable) {
--- src/edu/monash/fit2099/demo/mars/Stick.java
+++ src/edu/monash/fit2099/demo/mars/Stick.java
@@ -3,7 +3,6 @@ package edu.monash.fit2099.demo.mars;
 
 import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.WeaponItem;
-import game.CraftingAction;
 
 public class Stick extends WeaponItem {
 
